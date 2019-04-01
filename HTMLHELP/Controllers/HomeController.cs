d20e59525130507d4116fdd9af682fbd306cb7a5
Jonathan Santana <santanajonathan098@gmail.com>
2019-04-01 10:27:38 -0400

    primer commit

---
+++ HTMLHELP/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTMLHELP.Models;
+
+namespace HTMLHELP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+        
+
+        [HttpPost]
+        public ActionResult About(Formulario ff)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Proceso", ff);
+            }
+            else
+            {
+                return View("About");
+            }
+
+            
+        }
+
+        public ActionResult Proceso(Formulario ff)
+        {
+            //Formulario f = new Formulario();
+
+            //f.Cedula = int.Parse(Request.Form["Cedula"]);
+            //f.Nombre = Request.Form["Nombre"].ToString();
+            //f.Apellido = Request.Form["Apellido"];
+            //f.Edad = int.Parse(Request.Form["edad"]);
+            //f.Telefono = int.Parse(Request.Form["Telefono"]);
+            //f.Correo = Request.Form["Correo"];
+            //f.Genero = Request.Form["Genero"];
+
+            return View(ff);
+        }
+    }
+}
---
+++ HTMLHELP/Models/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace HTMLHELP.Models
+{
+    public class Formulario
+    {   
+        [Required(ErrorMessage ="Este campo es obligarotio")]
+        public int Cedula { get; set; }//not null
+
+        [Required(ErrorMessage = "Este campo es obligarotio")]
+        public string Nombre { get; set; }//not null
+
+        [Required(ErrorMessage = "Este campo es obligarotio")]
+        public string Apellido { get; set; }//not null
+
+        [Range(15,100,ErrorMessage = "La edad debe de ser mayor a 15 y menor de 100")]
+        public int Edad { get; set; }//validar
+
+        public int Telefono { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Este campo es obligarotio")]
+        public string Correo { get; set; }//validar
+
+        public Genero Genero { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string EstadoCivil { get; set; }
+
+        public bool Hobbys1 { get; set; }
+        public bool Hobbys2 { get; set; }
+        public bool Hobbys3 { get; set; }
+        public bool Hobbys4 { get; set; }
+
+
+
+    }
+
+    
+    public enum Genero
+    {
+        hombre, mujer
+    }
+}
